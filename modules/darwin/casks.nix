83a2481e9275ca6ad5dadc3be82f07334b633e2e
William Goeller <william@williamgoeller.com>
2023-12-17 14:54:38 -0500

    _

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,38 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  # "insomnia"
+  # "ngrok"
+  # "postico"
+  "visual-studio-code"
+
+  # Communication Tools
+  "discord"
+  # "loom"
+  "notion"
+  "slack"
+  # "telegram"
+  "zoom"
+
+  # Utility Tools
+  "appcleaner"
+  "syncthing"
+
+  # Entertainment Tools
+  "steam"
+  "vlc"
+
+  # Productivity Tools
+  # "raycast"
+  # "asana"
+
+  # Browsers
+  "google-chrome"
+  "firefox"
+  "firefox-developers-edition"
+
+  # AI
+  # "diffusionbee"
+]
