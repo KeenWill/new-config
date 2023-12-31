45b5edcf566f4f2cc6b7350b459304758565823b
William Goeller <william@williamgoeller.com>
2023-12-31 12:42:28 -0500

    _

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -60,6 +60,22 @@ let user = "williamgoeller";
 
   services.tailscale.enable = true;
 
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://localhost:3000";
+    notificationSender = "hydra@localhost";
+    # buildMachinesFiles = [];
+    # useSubstitutes = true;
+  };
+
+  nix.buildMachines = [
+    { hostName = "localhost";
+      system = "x86_64-linux";
+      supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+      maxJobs = 8;
+    }
+  ];
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
