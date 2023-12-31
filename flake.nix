fc913bf2e925c048c45e36edade91a086062bffc
William Goeller <william@williamgoeller.com>
2023-12-31 16:27:04 -0500

    _

--- flake.nix
+++ flake.nix
@@ -47,7 +47,6 @@
       darwinSystems = [ "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) f;
 
-
       devShell = system: let pkgs = nixpkgs.legacyPackages.${system}; in {
         default = with pkgs; mkShell {
           nativeBuildInputs = with pkgs; [ 
