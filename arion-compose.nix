ecb9a126e47feecd91ff9bc4cbb9e0deceb1bf3e
William Goeller <william@williamgoeller.com>
2023-12-30 21:38:31 -0500

    _

--- arion-compose.nix
+++ arion-compose.nix
@@ -127,36 +127,36 @@ in
       };
     };
 
-    plex-server = {
-      service = {
-        container_name = "plex-server";
-        image = "linuxserver/plex:latest";
-        restart = "unless-stopped";
-        environment = { 
-            PUID = puid;
-            PGID = pgid;
-            TZ = tz;
-            VERSION = "docker";
-            PLEX_CLAIM = "claim-fUUAqeFd8wPgy73c1M1Y";
-        };
-        volumes = [ 
-            "${root}/config/plex:/config" # plex config
-            "${root}/complete/movies:/movies"
-            "${root}/complete/tv:/tv"
-        ];
-        ports = [
-            "32400:32400/tcp"
-            "8324:8324/tcp"
-            "32469:32469/tcp"
-            "1900:1900/udp"
-            "32410:32410/udp"
-            "32412:32412/udp"
-            "32413:32413/udp"
-            "32414:32414/udp"
-        ];
-        networks = ["htpc"];
-      };
-    };
+    # plex-server = {
+    #   service = {
+    #     container_name = "plex-server";
+    #     image = "linuxserver/plex:latest";
+    #     restart = "unless-stopped";
+    #     environment = { 
+    #         PUID = puid;
+    #         PGID = pgid;
+    #         TZ = tz;
+    #         VERSION = "docker";
+    #         PLEX_CLAIM = "claim-fUUAqeFd8wPgy73c1M1Y";
+    #     };
+    #     volumes = [ 
+    #         "${root}/config/plex:/config" # plex config
+    #         "${root}/complete/movies:/movies"
+    #         "${root}/complete/tv:/tv"
+    #     ];
+    #     ports = [
+    #         "32400:32400/tcp"
+    #         "8324:8324/tcp"
+    #         "32469:32469/tcp"
+    #         "1900:1900/udp"
+    #         "32410:32410/udp"
+    #         "32412:32412/udp"
+    #         "32413:32413/udp"
+    #         "32414:32414/udp"
+    #     ];
+    #     networks = ["htpc"];
+    #   };
+    # };
 
     bazarr = {
       service = {
